adf724864865f7e8652e8faf5c9b22af28316ea1
Ishani <ishaniist@gmail.com>
2021-09-16 18:56:22 +0530

    Generate ObjectID s using nodejs

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,20 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb")
-const MongoClient = mongodb.MongoClient
+// const mongodb = require("mongodb")
+// const MongoClient = mongodb.MongoClient
+// const ObjectId = mongodb.ObjectId
+
+const {MongoClient, ObjectId} = require ('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1/27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectId()  // generate new ids
+// console.log(id)
+console.log(id.id)
+console.log(id.id.length)
+console.log(id.toHexString().length)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
     if(error){
@@ -15,8 +25,9 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
     const db = client.db(databaseName)
 
     // db.collection('users').insertOne({
-    //     name: 'Ishani',
-    //     age: '28'
+    //  //   _id:id,
+    //     name: 'Sakura',
+    //     age: '25'
     // }, (error, result)=>{
     //     if(error){
     //        return console.log('Unable to insert user')
@@ -39,22 +50,22 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
     //     console.log(result.insertedIds)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description:'Clean the house',
-            completed:true
-        },{
-            description:'Renew inspection',
-            completed : false
-        },{
-            description:'Pot plants',
-            completed: false
-        }
-    ],(error,result)=>{
-        if (error) {
-            return console.log('Unable to insert tasks')
-        }
-        console.log(result.insertedIds)
-    })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description:'Clean the house',
+    //         completed:true
+    //     },{
+    //         description:'Renew inspection',
+    //         completed : false
+    //     },{
+    //         description:'Pot plants',
+    //         completed: false
+    //     }
+    // ],(error,result)=>{
+    //     if (error) {
+    //         return console.log('Unable to insert tasks')
+    //     }
+    //     console.log(result.insertedIds)
+    // })
 
 })
