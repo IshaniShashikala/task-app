cab7fc82b5cb14ffbd70b8912c1bd474f5b9a7cc
Ishani <ishaniist@gmail.com>
2021-09-18 09:27:29 +0530

    Querying Documents

--- mongodb.js
+++ mongodb.js
@@ -10,11 +10,6 @@ const connectionURL = 'mongodb://127.0.0.1/27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectId()  // generate new ids
-// console.log(id)
-console.log(id.id)
-console.log(id.id.length)
-console.log(id.toHexString().length)
-console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
     if(error){
@@ -24,48 +19,45 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true},(error, client)=>{
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //  //   _id:id,
-    //     name: 'Sakura',
-    //     age: '25'
-    // }, (error, result)=>{
-    //     if(error){
-    //        return console.log('Unable to insert user')
+    // db.collection('users').findOne({name:'Jen'},(error,user)=>{
+    // db.collection('users').findOne({name:'Jen', age:1},(error,user)=>{ //gives null
+    // db.collection('users').findOne({_id:"61422b25269cdae3ecf827b0"},(error,user)=>{ //_id is binary value - so gives null
+    // db.collection('users').findOne({_id:new ObjectId("61422b25269cdae3ecf827b0")},(error,user)=>{    
+    //     if (error) {
+    //         return console.log('Unable to fetch')
     //     }
-    //     console.log(result.insertedId)
+
+    //     console.log(user)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 26
-    //      },{
-    //         name: 'Jone',
-    //         age:27
-    //     }
-    // ],(error,result)=>{
+    // db.collection('users').find({age:27}).toArray((error,users)=>{
     //     if (error) {
-    //         return console.log('Unable to insert documents')
+    //         return console.log('Unable to fetch')
     //     }
-    //     console.log(result.insertedIds)
+
+    //     console.log(users)
     // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description:'Clean the house',
-    //         completed:true
-    //     },{
-    //         description:'Renew inspection',
-    //         completed : false
-    //     },{
-    //         description:'Pot plants',
-    //         completed: false
-    //     }
-    // ],(error,result)=>{
+    // db.collection('users').find({age:27}).count((error,count)=>{
     //     if (error) {
-    //         return console.log('Unable to insert tasks')
+    //         return console.log('Unable to fetch')
     //     }
-    //     console.log(result.insertedIds)
+
+    //     console.log(count)
     // })
+
+    db.collection('tasks').findOne({_id:new ObjectId('6142301ff28ac40faa7e73f3')},(error,task)=>{
+        if (error) {
+            return console.log('Unable to fetch')
+        }
+        console.log(task)
+    })
+
+    db.collection('tasks').find({completed:false}).toArray((error,tasks)=>{
+        if (error) {
+            return console.log('Unable to fetch')
+        }
+        console.log(tasks)
+    })
 
 })
