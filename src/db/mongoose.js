143c10612a12c890ea11848599f20f46547a033c
Ishani <ishaniist@gmail.com>
2021-09-19 21:24:52 +0530

    Data Validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     // useNewUrlParser: true, //thses are not supported. mongose 6 behaves these are true
@@ -7,16 +8,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 
 const User = mongoose.model('User',{
     name:{
-        type: String
+        type: String,
+        required: true,
+        trim:true
+    }, 
+    email:{
+        type:String,
+        required: true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength: 7,
+        trim:true,    
+        validate(value){
+            if (value.toLowerCase().includes('password')) {
+                 throw new Error('Password cannot contain "password"') 
+            }
+        }
     },
     age:{
-        type: Number
+        type: Number,
+        default:0,
+        validate(value){
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
+    
 })
 
 const me = new User({
-    name: 'Andrew',
-    age: 27
+    name:'    Emmy ',
+    email:'MYEMAIL@MEAD.IO',
+    password: 'phone121!'
 })
 
 me.save().then(()=>{
@@ -27,16 +60,18 @@ me.save().then(()=>{
 
 const Task = mongoose.model('Task', {
     description:{
-        type:String
+        type:String,
+        required:true,
+        trim:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     }
 })
 
 const task = new Task({
-    description:'Clean the floor',
-    completed: true
+   description:'  Eat Lunch'
 })
 
 task.save().then(()=>{
