9dc09c5ec018e82b99a0cfa028653a85b62cd1f7
Ishani <ishaniist@gmail.com>
2021-10-02 19:35:12 +0530

    Add Sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,13 +21,20 @@ router.post('/tasks',auth, async (req,res)=>{
 
 //GET /tasks?completed=true (all, false, true)
 //GET/tasks?limit=10&skip=20
+//GET/tasks?sortBy=createdAt:desc  ( sort or sortBy), _desc, _asc, :desc
 router.get('/tasks', auth, async(req,res)=>{
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true' // req.query.completed - this is a string value. not a boolean value
     }
 
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+       
     try {
         // const tasks = await Task.find({owner:req.user._id})
         await req.user.populate({
@@ -35,8 +42,12 @@ router.get('/tasks', auth, async(req,res)=>{
             match,
             options:{
                 limit:parseInt(req.query.limit),
-                skip:parseInt(req.query.skip)
-                // limit:2
+                skip:parseInt(req.query.skip),
+                sort
+                // sort:{
+                //     // createdAt: 1 //(desc -1)
+                //     //completed: 1
+                // }
             }
         })
         res.send(req.user.tasks)
