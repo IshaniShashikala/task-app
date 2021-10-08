c215496a1861ab1b665707422cd3ebdf31c8c91d
Ishani <ishaniist@gmail.com>
2021-10-08 23:26:06 +0530

    Send Welcome & Cancelation Emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,25 @@ const sendgridAPIKey = "SG.HpUpCxMxTsOLCleLE59TvA.3qaV5Fct9TUBgz4anYBqrLHUvkguuG
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to:'ishaniist@gmail.com',
-    from:'ishaniist@gmail.com',
-    subject:'This is my first creation',
-    text:'I hope this one actualy get to you.'
-})
+const sendWelcomeEmail = (email,name) =>{
+    sgMail.send({
+        to:email,
+        from:'ishaniist@gmail.com',
+        subject:'Thanks for joining in!',
+        text:'Welcome to the app, ${name}. Let me know you get along with the app'
+    })
+}
+
+const sendCancelationEmail = (email,name)=>{
+    sgMail.send({
+        to:email,
+        from:'ishaniist@gmail.com',
+        subject:'Sorry to see you go ',
+        text:'Goodbye, ${name}. I hope to see you back sometime soon'
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
