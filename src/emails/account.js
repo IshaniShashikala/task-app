94004f94dded547846bb1114ad10ec68907f12d3
Ishani <ishaniist@gmail.com>
2021-10-13 15:57:21 +0530

    Set Environment Variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,24 +1,56 @@
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 
-const sendgridAPIKey = "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"
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-sgMail.setApiKey(sendgridAPIKey)
+// const sendWelcomeEmail = (email, name) =>{
+//     sgMail.send({
+//         to:email,
+//         from:'ishani@liveroom.xyz',
+//         subject:'Thanks for joining in!',
+//         text:'Welcome to the app, ${name}. Let me know you get along with the app'
+//     })
+// }
 
-const sendWelcomeEmail = (email,name) =>{
-    sgMail.send({
-        to:email,
-        from:'ishaniist@gmail.com',
-        subject:'Thanks for joining in!',
-        text:'Welcome to the app, ${name}. Let me know you get along with the app'
+// const sendCancelationEmail = (email, name)=>{
+//     sgMail.send({
+//         to:email,
+//         from:'ishani@liveroom.xyz',
+//         subject:'Sorry to see you go ',
+//         text:'Goodbye, ${name}. I hope to see you back sometime soon'
+//     })
+// }
+
+
+function transporter() {
+    return nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user: '********@gmail.com', //Give email address
+            pass:'******'   //Give password (or special app password)
+        }
     })
 }
 
-const sendCancelationEmail = (email,name)=>{
-    sgMail.send({
-        to:email,
-        from:'ishaniist@gmail.com',
-        subject:'Sorry to see you go ',
-        text:'Goodbye, ${name}. I hope to see you back sometime soon'
+function sendWelcomeEmail(email, name) {
+    const transporterObject = transporter()
+    transporterObject.sendMail({
+        from: 'Task Manager API <ishani@liveroom.xyz>',
+        to: email,
+        subject: 'Thanks for joining!',
+        text: `Welcome to our service, ${name}!`,
+        html: `<b>Welcome to our service, ${name}!</b>`
+    })
+}
+ 
+function sendCancelationEmail(email, name) {
+    const transporterObject = transporter()
+    transporterObject.sendMail({
+        from: 'Task Manager API <ishani@liveroom.xyz>',
+        to: email,
+        subject: 'We\'re sorry to see you leave',
+        text: `We hope to see you back again someday, ${name}!`,
+        html: `<b>We hope to see you back again someday, ${name}!</b>`
     })
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -77,7 +77,7 @@ userSchema.methods.toJSON = function () {
 //Methods(instance methods) are accessible on instances- methods on the intance and individual user
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id:user._id.toString()},'thisismynewcourse')
+    const token = jwt.sign({_id:user._id.toString()},process.env.JWT_SECRET)
     
     user.tokens = user.tokens.concat({token})
     await user.save()
