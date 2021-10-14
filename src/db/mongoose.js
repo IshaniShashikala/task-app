58362434aa99d2cab114c3a95f2c69f73b598fae
Ishani <ishaniist@gmail.com>
2021-10-14 14:30:46 +0530

    Refactor Code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL,{
-    // useNewUrlParser: true, //thses are not supported. mongose 6 behaves these are true
-    // useCreateIndex: true
-    // useFindAndModify: false
+    
 })
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,5 +1,4 @@
 const sgMail = require('@sendgrid/mail')
-// const nodemailer = require('nodemailer')
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -20,39 +19,6 @@ const sendCancelationEmail = (email, name)=>{
         text:`Goodbye, ${name}. I hope to see you back sometime soon`
     })
 }
-
-
-// function transporter() {
-//     return nodemailer.createTransport({
-//         service:'gmail',
-//         auth:{
-//             user: '********@gmail.com', //Give email address
-//             pass:'******'   //Give password (or special app password)
-//         }
-//     })
-// }
-
-// function sendWelcomeEmail(email, name) {
-//     const transporterObject = transporter()
-//     transporterObject.sendMail({
-//         from: 'Task Manager API <ishani@liveroom.xyz>',
-//         to: email,
-//         subject: 'Thanks for joining!',
-//         text: `Welcome to our service, ${name}!`,
-//         html: `<b>Welcome to our service, ${name}!</b>`
-//     })
-// }
- 
-// function sendCancelationEmail(email, name) {
-//     const transporterObject = transporter()
-//     transporterObject.sendMail({
-//         from: 'Task Manager API <ishani@liveroom.xyz>',
-//         to: email,
-//         subject: 'We\'re sorry to see you leave',
-//         text: `We hope to see you back again someday, ${name}!`,
-//         html: `<b>We hope to see you back again someday, ${name}!</b>`
-//     })
-// }
 
 module.exports = {
     sendWelcomeEmail,
