0cfd3da8f91bee4403ddc3d100a1bdfebd156fc1
Ishani <ishaniist@gmail.com>
2021-10-15 19:09:10 +0530

    Test Asynchronous Code

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit} = require('../src/math')
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add} = require('../src/math')
 
 test('Should calculate total with tip',()=>{
     const total = calculateTip(10, .3)
@@ -23,4 +23,28 @@ test('Should convert Fahrenheit to Celsius', ()=>{
 test('Should convert Celsius to Fahrenheit',()=>{
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async Test Demo',(done)=>{
+//     setTimeout(()=>{
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+//this fails because of actual and expected value are different 
+
+test('Should add two numbers', (done)=>{
+    add(2,3).then((sum)=>{
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+//awit --> jest waits till everything done 
+//async function always return a promise
+//( jest waits for the promise to either fulfilles or rejected before figuring out if the test case is pass or fail )
+
+test('Should add two numbers async/await', async()=>{
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
